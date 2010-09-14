fe9a3864001d8edf7452fcf89815f37867c18ddc
Magnus Johansson <kkckkc@gmail.com>
2010-09-14 17:38:20 +0200

    Save all action

---
+++ editor/src/main/java/kkckkc/jsourcepad/action/FileSaveAllAction.java
@@ -0,0 +1,27 @@
+
+package kkckkc.jsourcepad.action;
+
+import java.awt.event.ActionEvent;
+import kkckkc.jsourcepad.model.Doc;
+import kkckkc.jsourcepad.model.Window;
+import kkckkc.jsourcepad.util.action.BaseAction;
+
+public class FileSaveAllAction extends BaseAction {
+
+	private FileSaveAction fileSaveAction;
+    private final Window window;
+
+	public FileSaveAllAction(Window window, FileSaveAction fileSaveAction) {
+		this.window = window;
+		this.fileSaveAction = fileSaveAction;
+        setActionStateRules(ActionStateRules.HAS_ACTIVE_DOC);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+        for (int i = 0; i < window.getDocList().getDocCount(); i++) {
+            window.getDocList().setActive(i);
+            fileSaveAction.actionPerformed(e);
+        }
+	}
+}
