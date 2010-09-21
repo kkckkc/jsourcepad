7eb0e41aa83c4092e6be84c11a6238933a0d8d90
Magnus Johansson <kkckkc@gmail.com>
2010-09-21 12:37:22 +0200

    Fix for Show/Hide Project Drawer name

--- editor/src/main/java/kkckkc/jsourcepad/action/ViewShowHideProjectDrawerAction.java
+++ editor/src/main/java/kkckkc/jsourcepad/action/ViewShowHideProjectDrawerAction.java
@@ -4,11 +4,14 @@ import kkckkc.jsourcepad.model.Application;
 import kkckkc.jsourcepad.model.SettingsManager;
 import kkckkc.jsourcepad.model.WindowSettings;
 import kkckkc.jsourcepad.util.action.BaseAction;
+import kkckkc.jsourcepad.util.action.Presenter;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 
-public class ViewShowHideProjectDrawerAction extends BaseAction {
+public class ViewShowHideProjectDrawerAction extends BaseAction implements Presenter.Menu {
     public ViewShowHideProjectDrawerAction() {
+        updateName();
     }
 
     @Override
@@ -18,5 +21,23 @@ public class ViewShowHideProjectDrawerAction extends BaseAction {
         WindowSettings ws = settingsManager.get(WindowSettings.class);
         ws.setShowProjectDrawer(! ws.isShowProjectDrawer());
         settingsManager.update(ws);
+
+        updateName();
+    }
+
+    @Override
+    public JMenuItem getMenuItem() {
+        updateName();
+        return new JMenuItem(this);
+    }
+
+    private void updateName() {
+        SettingsManager settingsManager = Application.get().getSettingsManager();
+        WindowSettings ws = settingsManager.get(WindowSettings.class);
+        if (ws.isShowProjectDrawer()) {
+            putValue(NAME, "Hide Project Drawer");
+        } else {
+            putValue(NAME, "Show Project Drawer");
+        }
     }
 }
