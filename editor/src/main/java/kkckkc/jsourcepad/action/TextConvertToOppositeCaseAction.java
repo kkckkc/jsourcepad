b02202eb44ba608ac039b76ab60f46a834accfa8
Magnus Johansson <kkckkc@gmail.com>
2010-09-22 19:43:14 +0200

    Text > Convert > to Titlecase and to Opposite Case

---
+++ editor/src/main/java/kkckkc/jsourcepad/action/TextConvertToOppositeCaseAction.java
@@ -0,0 +1,39 @@
+package kkckkc.jsourcepad.action;
+
+import kkckkc.jsourcepad.model.Buffer;
+import kkckkc.jsourcepad.model.Window;
+import kkckkc.jsourcepad.util.action.BaseAction;
+import kkckkc.syntaxpane.model.Interval;
+
+import java.awt.event.ActionEvent;
+
+public class TextConvertToOppositeCaseAction extends BaseAction {
+    private final Window window;
+
+	public TextConvertToOppositeCaseAction(Window window) {
+		this.window = window;
+        setActionStateRules(ActionStateRules.HAS_ACTIVE_DOC);
+	}
+
+	@Override
+    public void actionPerformed(ActionEvent e) {
+        Buffer b = window.getDocList().getActiveDoc().getActiveBuffer();
+
+        Interval iv = b.getSelectionOrCurrentLine();
+        String text = b.getText(iv);
+
+        char[] chars = text.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (Character.isLetter(chars[i])) {
+                if (Character.isUpperCase(chars[i])) {
+                    chars[i] = Character.toLowerCase(chars[i]);
+                } else {
+                    chars[i] = Character.toUpperCase(chars[i]);
+                }
+            }
+        }
+
+        b.replaceText(iv, new String(chars), null);
+    }
+
+}
---
+++ editor/src/main/java/kkckkc/jsourcepad/action/TextConvertToTitlecaseAction.java
@@ -0,0 +1,43 @@
+package kkckkc.jsourcepad.action;
+
+import kkckkc.jsourcepad.model.Buffer;
+import kkckkc.jsourcepad.model.Window;
+import kkckkc.jsourcepad.util.action.BaseAction;
+import kkckkc.syntaxpane.model.Interval;
+
+import java.awt.event.ActionEvent;
+
+public class TextConvertToTitlecaseAction extends BaseAction {
+    private final Window window;
+
+	public TextConvertToTitlecaseAction(Window window) {
+		this.window = window;
+        setActionStateRules(ActionStateRules.HAS_ACTIVE_DOC);
+	}
+
+	@Override
+    public void actionPerformed(ActionEvent e) {
+		Buffer b = window.getDocList().getActiveDoc().getActiveBuffer();
+
+		Interval iv = b.getSelectionOrCurrentLine();
+        String text = b.getText(iv);
+
+        boolean inword = false;
+        char[] chars = text.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (Character.isLetter(chars[i])) {
+                if (! inword) {
+                    chars[i] = Character.toTitleCase(chars[i]);
+                } else {
+                    chars[i] = Character.toLowerCase(chars[i]);
+                }
+                inword = true;
+            } else {
+                inword = false;
+            }
+        }
+
+		b.replaceText(iv, new String(chars), null);
+    }
+
+}
