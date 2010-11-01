c6445b945861397dadc8d5c836be12a03a1aeeca
Magnus Johansson <kkckkc@gmail.com>
2010-11-01 15:53:53 +0100

    Tab completion when line ends with whitespace

--- editor/src/main/java/kkckkc/jsourcepad/model/bundle/snippet/SnippetUtils.java
+++ editor/src/main/java/kkckkc/jsourcepad/model/bundle/snippet/SnippetUtils.java
@@ -4,11 +4,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SnippetUtils {
-	public static final Pattern LAST_WORD_PATTERN = Pattern.compile("(^|\\s+|.*\n)(\\S+)$", Pattern.DOTALL);
+	public static final Pattern LAST_WORD_PATTERN = Pattern.compile("(^|\\s+|.*\n)(\\S+)(\\s*)$", Pattern.DOTALL);
 	
 	public static String getSnippet(String s) {
 		Matcher matcher = LAST_WORD_PATTERN.matcher(s);
 		if (! matcher.matches()) return "";
-		else return matcher.group(2);
+		else return matcher.group(2).trim();
 	}
 }
