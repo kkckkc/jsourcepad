5200aa8d01c89503699513d224cc4d8715355c03
Magnus Johansson <kkckkc@gmail.com>
2010-11-11 15:42:32 +0100

    Changed bat and CHANGELOG

--- editor-package/zip/contents/jsourcepad.bat
+++ editor-package/zip/contents/jsourcepad.bat
@@ -1,1 +1,1 @@
-java -client -Xms32m -Xrs -XX:+UseTLAB -Xverify:none -noclassgc -Xshare:on -Djava.util.logging.config.class=kkckkc.jsourcepad.BootstrapLogger -classpath "@CLASSPATH@" kkckkc.jsourcepad.Bootstrap
+java -client -Xms32m -Xrs -XX:+UseTLAB -Xverify:none -noclassgc -Xshare:on -Djava.util.logging.config.class=kkckkc.jsourcepad.BootstrapLogger -classpath "@CLASSPATH@" kkckkc.jsourcepad.Bootstrap %1
