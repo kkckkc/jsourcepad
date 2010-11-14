328ef4835a3bf29bc16d39bb73677424c0cd3de9
Magnus Johansson <kkckkc@gmail.com>
2010-11-14 20:33:10 +0100

    - Edit with JSourcepad  - File Association dialog registers bat if launched with bat file and exe if launched with exe file

--- editor-package/zip/contents/jsourcepad.bat
+++ editor-package/zip/contents/jsourcepad.bat
@@ -1,1 +1,2 @@
-java -client -Xms32m -Xrs -XX:+UseTLAB -Xverify:none -noclassgc -Xshare:on -Djava.util.logging.config.class=kkckkc.jsourcepad.BootstrapLogger -classpath "@CLASSPATH@" kkckkc.jsourcepad.Bootstrap %1
+cd %~dp0
+javaw -client -Xms32m -Xrs -XX:+UseTLAB -Xverify:none -noclassgc -Xshare:on -DexecutableName=jsourcepad.bat -Djava.util.logging.config.class=kkckkc.jsourcepad.BootstrapLogger -classpath "@CLASSPATH@" kkckkc.jsourcepad.Bootstrap %1
