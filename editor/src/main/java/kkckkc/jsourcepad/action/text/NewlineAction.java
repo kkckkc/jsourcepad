4c906264a5d110965abd7322b7f12a106863e64f
Magnus Johansson <kkckkc@gmail.com>
2011-01-17 21:18:40 +0100

    Changed newline handling to only have special handling for braces

--- editor/src/main/java/kkckkc/jsourcepad/action/text/NewlineAction.java
+++ editor/src/main/java/kkckkc/jsourcepad/action/text/NewlineAction.java
@@ -4,12 +4,10 @@ import kkckkc.jsourcepad.model.Application;
 import kkckkc.jsourcepad.model.Buffer;
 import kkckkc.jsourcepad.model.Doc;
 import kkckkc.jsourcepad.model.InsertionPoint;
-import kkckkc.jsourcepad.model.bundle.PrefKeys;
 import kkckkc.jsourcepad.util.action.BaseAction;
 import kkckkc.syntaxpane.model.Interval;
 
 import java.awt.event.ActionEvent;
-import java.util.List;
 
 public final class NewlineAction extends BaseAction {
     public void performAction(ActionEvent e) {
@@ -18,25 +16,19 @@ public final class NewlineAction extends BaseAction {
     	InsertionPoint ip = doc.getActiveBuffer().getInsertionPoint();
 
         Buffer buffer = doc.getActiveBuffer();
-        InsertionPoint insertionPoint = buffer.getInsertionPoint();
 
-        boolean inSmartTypingPair = false;
+        boolean specialHandlingForBraces = false;
 
         if (ip.getPosition() > 0) {
-            List<List<String>> pairs = (List) Application.get().getBundleManager().getPreference(
-                    PrefKeys.PAIRS_SMART_TYPING, insertionPoint.getScope());
-
             String charToTheRight = buffer.getText(Interval.createWithLength(ip.getPosition(), 1));
             String charToTheLeft = buffer.getText(Interval.createWithLength(ip.getPosition() - 1, 1));
 
-            for (List<String> pair : pairs) {
-                if (pair.get(0).equals(charToTheLeft) && pair.get(1).equals(charToTheRight)) {
-                    inSmartTypingPair = true;
-                }
+            if ("{".equals(charToTheLeft) && "}".equals(charToTheRight)) {
+                specialHandlingForBraces = true;
             }
         }
 
-        if (inSmartTypingPair) {
+        if (specialHandlingForBraces) {
             buffer.insertText(ip.getPosition(), "\n\n", null);
             buffer.indent(Interval.createWithLength(ip.getPosition(), 2));
             buffer.setSelection(Interval.createEmpty(buffer.getInsertionPoint().getPosition() - 1));
