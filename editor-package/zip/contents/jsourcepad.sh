a39198fa482912259fbadc852a810bf8a92b7db7
Magnus Johansson <kkckkc@gmail.com>
2011-03-01 14:02:10 +0100

    Fixed current folder issue

--- editor-package/zip/contents/jsourcepad.sh
+++ editor-package/zip/contents/jsourcepad.sh
@@ -3,4 +3,4 @@
 _path_of_script="$(cd "${0%/*}" 2>/dev/null; echo "$PWD"/"${0##*/}")"
 _pwd=`dirname "$_path_of_script"`
 
-java -client -Xms32m -Xmx256m -Xrs -Xverify:none -Djava.util.logging.config.class=kkckkc.jsourcepad.BootstrapLogger -noclassgc -classpath "@UNIXCLASSPATH@" kkckkc.jsourcepad.Bootstrap $@
+java -client -Xms32m -Xmx256m -Xrs -Xverify:none -Dapplication.folder=$_pwd -Djava.util.logging.config.class=kkckkc.jsourcepad.BootstrapLogger -noclassgc -classpath "@UNIXCLASSPATH@" kkckkc.jsourcepad.Bootstrap $@
