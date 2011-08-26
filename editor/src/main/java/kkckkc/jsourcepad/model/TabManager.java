755e7299944fa1d78adaf66c42c7e35d1ca0d3a7
Magnus Johansson <kkckkc@gmail.com>
2011-08-26 12:12:34 +0200

    Cleanup

--- editor/src/main/java/kkckkc/jsourcepad/model/TabManager.java
+++ editor/src/main/java/kkckkc/jsourcepad/model/TabManager.java
@@ -4,10 +4,6 @@ public interface TabManager {
 	public String getFirstIndentionString(CharSequence line);
 	public int getTabCount(CharSequence line);
 	public String createIndent(int count);
-//	
 	public int getTabSize();
-//	public void setTabSize(int size);
-//
 	public boolean isSoftTabs();
-//	public void setSoftTabs(boolean selected);
 }
